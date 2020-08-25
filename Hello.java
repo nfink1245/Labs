e8fbe3a02983f5c9747a0fd20515d97df6de046c
nfink1245 <70226624+nfink1245@users.noreply.github.com>
2020-08-25 12:59:01 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/************************************************
+	Hello
+*************************************************
+	Nathan Fink
+	8/25/2020
+	CMSC 255 Section C90
+*************************************************/
+
+public class Hello {
+	public static void main(String[] args){
+		System.out.println("Nathan Fink");
+		System.out.println("Hello");
+		System.out.println("Introduction to the Course");
+		System.out.println("Cmsc 255 Section C90");
+		System.out.println();
+	}
+}
